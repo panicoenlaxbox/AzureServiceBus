8fb48852d0098ee92f2986deef4afcee2d300865
panicoenlaxbox <panicoenlaxbox@gmail.com>
2017-06-26 11:40:36 +0200

    Add project files.

---
+++ Receiver/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using Microsoft.ServiceBus;
+using Microsoft.ServiceBus.Messaging;
+
+namespace Receiver
+{
+    class Program
+    {
+        const string connectionString = "";
+        const string queueName = "";
+
+        static void Main(string[] args)
+        {
+           
+
+            //var client = QueueClient.CreateFromConnectionString(connectionString, queueName);
+            //client.OnMessage(message =>
+            //{
+            //    Console.WriteLine($"Message id: {message.MessageId}");
+            //    Console.WriteLine($"Message body: {message.GetBody<string>()}");
+            //});
+            //Console.ReadKey();
+            //client.Close();
+
+            ConsoleKeyInfo consoleKeyInfo;
+            do
+            {
+                ReadMessages();
+                Console.WriteLine("Waiting for an enter key to reading messages...");
+                consoleKeyInfo = Console.ReadKey();
+            } while (consoleKeyInfo.Key == ConsoleKey.Enter);
+        }
+
+        private static void ReadMessages()
+        {
+            Console.WriteLine("Reading...");
+            var client = QueueClient.CreateFromConnectionString(connectionString, queueName);
+            bool queueStillHasMessages = true;
+            while (queueStillHasMessages)
+            {
+                BrokeredMessage message = client.Receive();
+                if (message != null)
+                {
+                    Console.WriteLine($"Message id: {message.MessageId}");
+                    Console.WriteLine($"Message body: {message.GetBody<string>()}");
+                    message.Complete();
+                }
+                else
+                {
+                    queueStillHasMessages = false;
+                }
+            }
+            client.Close();
+        }
+    }
+}
---
+++ Sender/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.ServiceBus.Messaging;
+
+namespace Sender
+{
+    class Program
+    {
+        const string connectionString = "";
+        const string queueName = "";
+
+        static void Main(string[] args)
+        {
+            ConsoleKeyInfo consoleKeyInfo;
+            bool send = true;
+            do
+            {
+                Console.WriteLine("Waiting for an enter key to sending a new message...");
+                consoleKeyInfo = Console.ReadKey();
+                if (consoleKeyInfo.Key == ConsoleKey.Enter)
+                    SendMessage();
+                else
+                    send = false;
+            } while (send);
+        }
+
+        private static void SendMessage()
+        {
+            var client = QueueClient.CreateFromConnectionString(connectionString, queueName);
+            var message = new BrokeredMessage($"This is a test message {DateTime.Now}!");
+
+            client.Send(message);
+            client.Close();
+        }
+    }
+}
